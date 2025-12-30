01456f23b1ec83bf4ef8d7a6e33cf1902a5b9d1a
Kristian Pessoa <sousakristian45@gmail.com>
2025-12-29 21:46:25 -0300

    adicionadas classes de security

---
+++ src/main/java/com/kristian/agendador_tarefas/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.kristian.agendador_tarefas.infrastructure.repository;
+
+import com.kristian.aprendendo_spring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/kristian/agendador_tarefas/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.kristian.agendador_tarefas.infrastructure.repository;
+
+import com.kristian.agendador_tarefas.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
